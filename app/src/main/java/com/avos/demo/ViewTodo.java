0c031448b0d5bca13b3a05a647488ac640f124cf
socrateszhang <sophiezhanguiuc@gmail.com>
2016-07-13 11:19:34 +0800

    -FIRST RUNNABLE VERSION 7/13/2016 -New Feature:
    -Included taking photo/ select from gallery functionality
    -added location, phone number, contact name fields
    -added back, new, and exit button

--- app/src/main/java/com/avos/demo/ViewTodo.java
+++ app/src/main/java/com/avos/demo/ViewTodo.java
@@ -1,4 +1,7 @@
+
 package com.avos.demo;
+
+//Detailed Page for product
 
 import android.app.Activity;
 import android.content.Intent;
@@ -8,10 +11,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.avos.avoscloud.AVAnalytics;
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.GetCallback;
 import com.avos.avoscloud.SaveCallback;
@@ -21,8 +26,12 @@ public class ViewTodo extends Activity {
   private TextView nameText;
   private TextView priceNumber;
   private TextView contentText;
+  private TextView contactName;
+  private TextView phone;
+  private TextView location;
   private String objectId;
   private AVImageView imgView;
+  private Button backBtn;
 
 
 
@@ -52,6 +61,20 @@ public class ViewTodo extends Activity {
     contentText = (TextView) findViewById(R.id.content);
     nameText = (TextView) findViewById(R.id.name);
     priceNumber = (TextView) findViewById(R.id.price);
+    contactName = (TextView) findViewById(R.id.storename);
+    phone = (TextView) findViewById(R.id.storephone);
+    location = (TextView) findViewById(R.id.location);
+
+
+    imgView =(AVImageView)findViewById(R.id.img);
+    backBtn=(Button)findViewById(R.id.backBtn);
+    backBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
+
 
     Intent intent = getIntent();
     // 通过搜索结果打开
@@ -70,12 +93,22 @@ public class ViewTodo extends Activity {
               String name = todo.getString("name");
               String price = todo.getString("price");
               String content = todo.getString("content");
+              String storeName = todo.getString("contactName");
+              String phonenum = todo.getString("phoneNum");
+              String local = todo.getString("location");
+
+              AVFile file = todo.getAVFile("img");
 
               if (content != null) {
                 contentText.setText(content);
                 priceNumber.setText(price);
                 nameText.setText(name);
+                contactName.setText(storeName);
+                phone.setText(phonenum);
+                location.setText(local);
 
+                imgView.setAVFile(file);
+                imgView.loadInBackground();
               }
             }
           }
@@ -91,21 +124,42 @@ public class ViewTodo extends Activity {
         String content = extras.getString("content");
         objectId = extras.getString("objectId");
 
-        //Todo product = new Todo();
-        //product.setObjectId(objectId);
-        // 通过Fetch获取content内容
-        //product.fetchIfNeededInBackground();
+        // 获取objectId
+        GetCallback<AVObject> getCallback=new GetCallback<AVObject>() {
+          @Override
+          public void done(AVObject todo, AVException arg1) {
+            if (todo != null) {
+              //String name = todo.getString("name");
+              //String price = todo.getString("price");
+              //String content = todo.getString("content");
+              String storeName = todo.getString("contactName");
+              String phonenum = todo.getString("phoneNum");
+              String local = todo.getString("location");
 
-        //AVFile file = product.getImg();
-        //imgView.setAVFile(file);
-        //imgView.loadInBackground();
+              //  contentText.setText(content);
+              //   priceNumber.setText(price);
+              //   nameText.setText(name);
+              contactName.setText(storeName);
+              phone.setText(phonenum);
+              location.setText(local);
+
+
+              AVFile file = todo.getAVFile("img");
+              imgView.setAVFile(file);
+              imgView.loadInBackground();
+            }
+          }
+        };
+        AVService.fetchTodoById(objectId, getCallback);
 
 
         if (content != null) {
           contentText.setText(content);
           priceNumber.setText(price);
           nameText.setText(name);
+
         }
+
       }
     }
 
